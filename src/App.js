301c4d11a7c6236b2442040640f28112e293653e
Bredy Evans <Bredell Evans>
2021-11-18 22:25:10 -0500

    canvas board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
+import CanvasDraw from "react-canvas-draw";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brushRadius: 2,
+      canvasWidth: 400,
+      canvasHeight: 320,
+      lazyRadius: 0,
+      hideGridX: true,
+      hideGridY: true,
+    };
+  }
+
+  isMobileOrNot = () => {
+    let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      /* your code here */
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <CanvasDraw
+          className="canvas"
+          brushRadius={this.state.brushRadius}
+          canvasWidth={this.state.canvasWidth}
+          canvasHeight={this.state.canvasHeight}
+          lazyRadius={this.state.lazyRadius}
+          hideGridX={this.state.hideGridX}
+          hideGridY={this.state.hideGridY}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
