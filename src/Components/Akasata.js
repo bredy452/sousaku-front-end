c943ca6b7beb1232369cd6451397e35a1310e8dc
Bredy Evans <Bredell Evans>
2021-11-26 14:56:42 -0500

    changed picture

--- src/Components/Akasata.js
+++ src/Components/Akasata.js
@@ -8,15 +8,15 @@ import akasata from "../Assets/akasata.png"
 export default function Akasata (props) {
     return (
         <>
-        <Container className="akasataAndTextDiv" >
-            <Row xs="auto" className="introduction">
-                <Col>
+        <div className="akasataAndTextDiv" >
+            {/* <Row xs="auto" className="introduction">
+                <Col> */}
                     <Image className="akasata"
                         src={akasata}
-                        width={211.35}
-                        height={1078} 
+                        width={1322}
+                        // height={1078} 
                     />
-                </Col>
+                {/* </Col>
                 <Col sm={9}>
                     <h3>
                         To learn Hiragana is to create a foundation for the rest of your Japanese.<br></br> By learning Hiragana, you will learn to the basics of Japanese writing.<br></br> It will also open doors to the Japanese resources you can use.
@@ -29,8 +29,8 @@ export default function Akasata (props) {
                     </h3>
                     
                 </Col>
-            </Row>
-        </Container>
+            </Row> */}
+        </div>
         </>
     )
 }
